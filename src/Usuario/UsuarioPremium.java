b7443d5ea1dfd2e9048e43cbf39236ce8fff90cc
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2021-04-14 23:10:10 -0300

    update main

--- src/Usuario/UsuarioPremium.java
+++ src/Usuario/UsuarioPremium.java
@@ -1,34 +1,13 @@
 package Usuario;
 
-public class UsuarioPremium extends Usuario{
-    private double mensalidade;
-    private String venciento;
-
-
-
+public class  UsuarioPremium extends Usuario {
 
 
     /////////////// Constructor //////////////////
-    public UsuarioPremium(String nome, String dtNascimento, double mensalidade, String venciento) {
+    public UsuarioPremium(String nome, String dtNascimento) {
         super(nome, dtNascimento);
-        this.mensalidade = mensalidade;
-        this.venciento = venciento;
     }
 
     /////////////// setters  && getters //////////////////
-    public double getMensalidade() {
-        return mensalidade;
-    }
 
-    public void setMensalidade(double mensalidade) {
-        this.mensalidade = mensalidade;
-    }
-
-    public String getVenciento() {
-        return venciento;
-    }
-
-    public void setVenciento(String venciento) {
-        this.venciento = venciento;
-    }
 }
