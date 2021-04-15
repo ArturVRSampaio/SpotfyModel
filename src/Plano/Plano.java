70e35843ad57694d8179998a91932e71bdcd584b
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2021-04-14 23:08:05 -0300

    extraindo valor de usuario para plano

---
+++ src/Plano/Plano.java
@@ -0,0 +1,11 @@
+package Plano;
+
+import Usuario.Usuario;
+import Usuario.UsuarioPremium;
+
+public class Plano{
+    private String nomePlano;
+    private double valorPlano;
+    private int diaVencimento;
+
+}
