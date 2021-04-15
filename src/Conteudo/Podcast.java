15d00bd0260dfd92b0f193b700ef304801e6db42
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2021-04-15 09:43:05 -0300

    Criando constructors

--- src/Conteudo/Podcast.java
+++ src/Conteudo/Podcast.java
@@ -3,13 +3,18 @@ package Conteudo;
 import Conjunto.Temporada;
 import Criador.PodCaster;
 
+import java.util.Calendar;
 import java.util.List;
 
-public class Podcast  extends Conteudo{
+public class Podcast extends Conteudo{
     private PodCaster podCaster;
     private List<Temporada> listaTemporada;
     private String Descricao;
 
-
-
+    public Podcast(String nome, Calendar dtCriacao, String urlImagem, PodCaster podCaster, List<Temporada> listaTemporada, String descricao) {
+        super(nome, dtCriacao, urlImagem);
+        this.podCaster = podCaster;
+        this.listaTemporada = listaTemporada;
+        Descricao = descricao;
+    }
 }
