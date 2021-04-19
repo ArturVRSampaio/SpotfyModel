0a5b0b29d64a3dd3f8b32c6ded43060b457303dc
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2021-04-19 08:21:11 -0300

    criando tipo de midia

---
+++ src/Conteudo/Midia/Propaganda.java
@@ -0,0 +1,12 @@
+package Conteudo.Midia;
+
+import java.util.Calendar;
+
+public class Propaganda extends Midia{
+
+    public Propaganda(Calendar dtCriacao, String nome, String urlPagina, String urlImagem, Calendar dtLancamento, String duracao) {
+        super(dtCriacao, nome, urlPagina, urlImagem, dtLancamento, duracao);
+    }
+
+
+}
