e9aae68ae4c349a5608ecd6e600280a6429aaa2c
ArturVRSampaio <artur.vitorio@astisoftware.com.br>
2021-04-19 01:37:25 -0300

    correcao constructor

--- src/Conteudo/Midia/Podcast.java
+++ src/Conteudo/Midia/Podcast.java
@@ -4,13 +4,12 @@ package Conteudo.Midia;
 import Conteudo.Conjunto.Podcast.Temporada;
 
 import java.util.Calendar;
-import java.util.List;
 
 public class Podcast extends Midia {
     private Temporada temporada;
 
     public Podcast(Calendar dtCriacao, String nome, String urlPagina, String urlImagem, Calendar dtLancamento, String duracao, Temporada temporada) {
         super(dtCriacao, nome, urlPagina, urlImagem, dtLancamento, duracao);
-        Temporada = temporada;
+        this.temporada = temporada;
     }
 }
